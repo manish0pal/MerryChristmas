efafd772d6a4261863dbc5887f6dc1b97e70d7f5
Manish Pal <62168967+manish0pal@users.noreply.github.com>
2020-12-16 18:42:17 +0530

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,80 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+tl.to(".intro", { y: "-100%", duration: 1.5, delay: 1.5},);
+
+
+var queryString = decodeURIComponent(window.location.search);
+queryString = queryString.substring(1);
+if(queryString==""){
+        queryString="Well Wisher"
+}
+document.getElementById('setUname').innerHTML = queryString;
+
+function sharebtn(){
+        var blur =document.getElementById('card')
+        var popup =document.getElementById('popup')
+        var btnblur = document.getElementById('shrbtn')
+        btnblur.classList.toggle('active')
+        popup.classList.toggle('active')
+        blur.classList.toggle('active')
+}
+function sendwhatsapp(){
+        console.log("whatsapp");
+        var letters = /^[A-Za-z]+$/;
+        var uname = document.getElementById('uname').value
+        if(uname == ""){
+                showsnaker()
+              }
+              else if(! uname.match(letters)){
+                showsnaker()
+              }
+              else{
+                      window.open('whatsapp://send?text=*Special Surprise only for you* :   '+window.location.href+"?"+uname)
+              }
+}
+function sendfacebook(){
+        var letters = /^[A-Za-z]+$/;
+        var uname =document.getElementById('uname').value
+        if(uname == ""){
+                showsnaker()
+              }
+              else if(! uname.match(letters)){
+                showsnaker()
+              }
+              else{
+                      window.open("https://www.facebook.com/sharer/sharer.php?u="+window.location.href+"?"+uname)
+              }
+}
+function showsnaker(){
+        var x = document.getElementById("snackbar");
+        x.classList.toggle('show');
+        console.log(x.classList);
+  setTimeout(function(){ x.classList.toggle('show');}, 3000);
+}
+
+var timer = new Date("Dec 24, 2020 24:00:00").getTime();
+
+  var run= setInterval(function() {
+  
+  var present = new Date().getTime();
+  
+  var inBetween= timer - present;
+  
+  var days = Math.floor(inBetween / (1000 * 60 * 60 * 24));
+  
+  var hours = Math.floor((inBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  
+  var minutes = Math.floor((inBetween % (1000 * 60 * 60)) / (1000 * 60));
+ 
+  
+  document.getElementById("days").innerHTML = days;
+  
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("mins").innerHTML = minutes;
+  
+if (inBetween < 0) {
+    clearInterval(run);
+  }
+}, 60);
+//Music
+document.getElementById("myAudio").play(); 
+
---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Let's Share</title>
+  
+    <link
+    href="https://fonts.googleapis.com/css2?family=Heebo:wght@500;700&family=Lobster&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+  <?php
+  $bg = array('img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg', 'img7.jpg','img8.jpg','img9.jpg' ); // array of filenames
+
+  $i = rand(0, count($bg)-1); // generate random number size of the array
+  $selectedBg = "$bg[$i]"; // set variable equal to which random filename was chosen
+  
+?>
+<style type="text/css">
+
+html { 
+    font: 14px/20px "Lato", Arial, sans-serif;
+    background: url(img/<?php echo $selectedBg; ?>) no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    height: 100%;
+  }
+</style>
+<link rel="stylesheet"   href="./indstyle.css">
+    <div class="slide-container" id="card">
+  
+        <div class="wrapper">
+          <div class="clash-card barbarian">
+            <div class="clash-card__image clash-card__image--barbarian">
+              <img src="./img/santa_crop.png" alt="barbarian" />
+            </div>
+            
+            <div class="clash-card__unit-name">Merry Christmas 
+              </div>
+            <div class="clash-card__unit-description">
+              in advance
+            </div>
+            <div class="clash-card__from">
+              from
+            </div>
+            <div  id = "setUname"class="clash-card__level clash-card__level--barbarian"></div>
+            <div class="clash-card__unit-stats clash-card__unit-stats--barbarian clearfix">
+              <div class="one-third">
+                <div class="stat" id="days">20</div>
+                <div class="stat-value">Day</div>
+              </div>
+      
+              <div class="one-third">
+                <div class="stat" id="hours">16</div>
+                <div class="stat-value">Hours</div>
+              </div>
+      
+              <div class="one-third no-border">
+                <div class="stat" id="mins">10</div>
+                <div class="stat-value">Min</div>
+              </div>
+      
+            </div>
+      
+          </div> <!-- end clash-card barbarian-->
+        </div> <!-- end wrapper -->
+        <br>
+        </div>
+
+            <!-- Code begins here -->
+        
+        <a onclick="sharebtn()"  id="shrbtn" class="float">
+          <i class="fa fa-share-alt my-float"></i>
+          </a>
+          <div class="intro">  </div>
+          <div id="snow-fall">  </div>
+
+          <div id="popup">
+            <div class="form">
+              <input type="text" id="uname" name="name" autocomplete="off" required>
+              <label for="name" class="label-name">
+                <span class="content-name">Your Name</span>
+              </label>
+            </div>
+            <div class="sharebuttondiv">
+            
+              <button class="wbutton" onclick="sharebtn() ,sendwhatsapp()"><i class="fa fa-whatsapp" aria-hidden="true"></i></button>
+      
+              <button class="fbutton" onclick="sharebtn(), sendfacebook()"><i class="fa fa-facebook" aria-hidden="true"></i></button>
+            </div>
+                 </div>
+                 <div id="snackbar" >Incorrect Name </div>
+ 
+                 <marquee id="movingsanta" behavior="scroll" direction="left">
+                  <img src="img/movingsatna.gif" class="img" height="50px" width="150px" alt="Ship">
+                </marquee>
+
+
+                <audio id="myAudio">
+                  <source src="./music/jingle-belwa.mp3" type="audio/mpeg">
+                 
+                </audio>
+	<!-- script -->
+  <script
+  src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"
+  integrity="sha512-IQLehpLoVS4fNzl7IfH8Iowfm5+RiMGtHykgZJl9AWMgqx0AmJ6cRWcB+GaGVtIsnC4voMfm8f2vwtY+6oPjpQ=="
+  crossorigin="anonymous"></script>
+   <script src="./ind.js"></script>
+    <script src="./particles.js"></script>
+    <script src="./app.js"></script>
+</body>
+</html>
+
